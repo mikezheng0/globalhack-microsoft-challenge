6d62157fb18eb33a0ee847086c86e65104728fdf
MikeZ <mike.zheng@mohawkcollege.ca>
2017-06-25 02:04:54 -0400

    Add first DB migration and Entities

---
+++ src/AngularJSSample/Migrations/20170625055218_InitialCreate.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace AngularJSSample.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Interaction",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CustomerId = table.Column<int>(nullable: false),
+                    Image = table.Column<byte[]>(nullable: true),
+                    Time = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Interaction", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Cordinates",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Height = table.Column<int>(nullable: false),
+                    InteractionId = table.Column<int>(nullable: false),
+                    Weight = table.Column<int>(nullable: false),
+                    X = table.Column<int>(nullable: false),
+                    Y = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cordinates", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Cordinates_Interaction_InteractionId",
+                        column: x => x.InteractionId,
+                        principalTable: "Interaction",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Emotions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Anger = table.Column<float>(nullable: false),
+                    Contempt = table.Column<float>(nullable: false),
+                    Disguest = table.Column<float>(nullable: false),
+                    Fear = table.Column<float>(nullable: false),
+                    Happiness = table.Column<float>(nullable: false),
+                    InteractionId = table.Column<int>(nullable: false),
+                    Nuetral = table.Column<float>(nullable: false),
+                    Sadness = table.Column<float>(nullable: false),
+                    Suprise = table.Column<float>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Emotions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Emotions_Interaction_InteractionId",
+                        column: x => x.InteractionId,
+                        principalTable: "Interaction",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cordinates_InteractionId",
+                table: "Cordinates",
+                column: "InteractionId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Emotions_InteractionId",
+                table: "Emotions",
+                column: "InteractionId",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cordinates");
+
+            migrationBuilder.DropTable(
+                name: "Emotions");
+
+            migrationBuilder.DropTable(
+                name: "Interaction");
+        }
+    }
+}
---
+++ src/AngularJSSample/Migrations/CameraContextModelSnapshot.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using AngularJSSample.Models;
+
+namespace AngularJSSample.Migrations
+{
+    [DbContext(typeof(CameraContext))]
+    partial class CameraContextModelSnapshot : ModelSnapshot
+    {
+        protected override void BuildModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.2")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("AngularJSSample.Models.Cordinates", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<int>("Height");
+
+                    b.Property<int>("InteractionId");
+
+                    b.Property<int>("Weight");
+
+                    b.Property<int>("X");
+
+                    b.Property<int>("Y");
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("InteractionId")
+                        .IsUnique();
+
+                    b.ToTable("Cordinates");
+                });
+
+            modelBuilder.Entity("AngularJSSample.Models.Emotions", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<float>("Anger");
+
+                    b.Property<float>("Contempt");
+
+                    b.Property<float>("Disguest");
+
+                    b.Property<float>("Fear");
+
+                    b.Property<float>("Happiness");
+
+                    b.Property<int>("InteractionId");
+
+                    b.Property<float>("Nuetral");
+
+                    b.Property<float>("Sadness");
+
+                    b.Property<float>("Suprise");
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("InteractionId")
+                        .IsUnique();
+
+                    b.ToTable("Emotions");
+                });
+
+            modelBuilder.Entity("AngularJSSample.Models.Interaction", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<int>("CustomerId");
+
+                    b.Property<byte[]>("Image");
+
+                    b.Property<DateTime>("Time");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Interaction");
+                });
+
+            modelBuilder.Entity("AngularJSSample.Models.Cordinates", b =>
+                {
+                    b.HasOne("AngularJSSample.Models.Interaction", "Interaction")
+                        .WithOne("Cordinates")
+                        .HasForeignKey("AngularJSSample.Models.Cordinates", "InteractionId")
+                        .OnDelete(DeleteBehavior.Cascade);
+                });
+
+            modelBuilder.Entity("AngularJSSample.Models.Emotions", b =>
+                {
+                    b.HasOne("AngularJSSample.Models.Interaction", "Interaction")
+                        .WithOne("Emotions")
+                        .HasForeignKey("AngularJSSample.Models.Emotions", "InteractionId")
+                        .OnDelete(DeleteBehavior.Cascade);
+                });
+        }
+    }
+}
